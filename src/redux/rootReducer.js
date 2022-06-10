4593279880991adead3a5879b65935cdccd59192
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-06-10 22:39:26 +0430

    usersReducer added to rootReducer & redux-think added to Store as midleWare

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import cakeReducer from './cake/cakeReducer';
 import iceCreamReducer from './iceCreame/iceCreamReducer';
+import usersReducer from './users/usersReducer';
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
+  users: usersReducer,
 });
 
 export default rootReducer;
